4b93f92ab9c877c7fa21225cae74290e443f86ed
Ethan Freire <element55star@gmail.com>
2019-03-12 12:37:30 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -9,3 +9,23 @@ function forLoop(array){
 }
 return array;
 }
+
+function whileLoop(n) {
+  let countdown = n;
+
+     while (countdown > 0) {
+      console.log(--countdown)
+    }
+    
+  }
+
+ function doWhileLoop(array) {
+  function maybeTrue() {
+    return Math.random() >= 0.5
+  }
+
+   do {
+    array.pop();
+  } while (array.length > -1 && maybeTrue());
+    return array;
+}
