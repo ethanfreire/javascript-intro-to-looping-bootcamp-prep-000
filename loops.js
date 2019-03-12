d5d988b3001add30ea88801921154286032024c9
Ethan Freire <element55star@gmail.com>
2019-03-12 12:52:28 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -19,13 +19,14 @@ function whileLoop(n) {
     return "done";
   }
 
- function doWhileLoop(array) {
-  function maybeTrue() {
-    return Math.random() >= 0.5
-  }
-
+ function doWhileLoop(num) {
+var i = 0;
+   function incrementVariable() {
+     i = i + 1;
+     return i;
+   }
+    
    do {
-    array.pop();
-  } while (array.length > -1 && maybeTrue());
-    return array;
+     console.log("I run once regardless");
+   } while (incrementVariable() < num);
 }
